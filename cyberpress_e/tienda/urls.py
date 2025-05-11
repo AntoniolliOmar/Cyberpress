3f946a048697b5f03427425401369364410a081f
omii <antoniolli.omar@gmail.com>
2025-05-11 19:51:36 -0300

    Cyberpress v1.0.0

--- cyberpress_e/tienda/urls.py
+++ cyberpress_e/tienda/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('productos/', views.lista_productos, name='lista_productos'),
     path('productos/<slug:slug>/', views.detalle_producto, name='detalle_producto'),
+    path('', views.index, name='index'),  # Esta línea define la URL para la página principal
+
 ]
--- cyberpress_e/tienda/views.py
+++ cyberpress_e/tienda/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from .models import Producto
-
+from django.shortcuts import render, get_object_or_404
+from django.urls import path
+from . import views
 
 
 def lista_productos(request):
@@ -9,17 +11,18 @@ def lista_productos(request):
     return render(request, 'tienda/lista_productos.html', context)
 
 
-from django.shortcuts import render, get_object_or_404
-from .models import Producto
 
 def detalle_producto(request, slug):
     producto = get_object_or_404(Producto, slug=slug, disponible=True)
     context = {'producto': producto}
     return render(request, 'tienda/detalle_producto.html', context)
 
+def index(request):
+    # Aquí puedes agregar lógica para obtener datos que quieras mostrar en la página principal
+    # Por ejemplo, los últimos productos, categorías destacadas, etc.
+    context = {}
+    return render(request, 'tienda/index.html', context)
 
-from django.urls import path
-from . import views
 
 urlpatterns = [
     path('productos/', views.lista_productos, name='lista_productos'),
